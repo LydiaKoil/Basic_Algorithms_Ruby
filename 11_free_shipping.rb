ecdda9a5a3e1afb193aad35c346e182499d71541
Lydtk <47219609+Lydtk@users.noreply.github.com>
2021-04-12 12:42:54 +0100

    refactored into one line

--- 11_free_shipping.rb
+++ 11_free_shipping.rb
@@ -2,9 +2,7 @@
 
 def free_shipping?(hash)
   sum = 0
-  hash.each do |_, value|
-    sum += value
-  end
+  hash.each { |_, value| sum += value }
   sum > 50
 end
 puts free_shipping?({ 'Monopoly' => 11.99, 'Secret Hitler' => 35.99, 'Bananagrams' => 13.99 })
