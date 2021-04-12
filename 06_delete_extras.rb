40b2b150fe4c16dc30306bc59e91605ab429a65a
Lydtk <47219609+Lydtk@users.noreply.github.com>
2021-04-12 10:34:30 +0100

    Create 06_delete_extras.rb

---
+++ 06_delete_extras.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+def delete_occurrences(arr, max)
+  hash = Hash.new(0)
+  new_arr = []
+  arr.each do |num|
+    hash[num] += 1
+    new_arr << num unless hash[num] > max
+  end
+
+  puts new_arr
+  puts hash
+end
+# delete_occurrences([1, 1, 1, 1], 2)
+# delete_occurrences([true, true, true], 3)
+delete_occurrences([13, true, 13, nil], 1)
