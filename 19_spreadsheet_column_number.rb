a01432f02b5a95d4c006351e5e4c7af0073bc265
Lydtk <47219609+Lydtk@users.noreply.github.com>
2021-04-19 10:00:30 +0100

    Update 19_spreadsheet_column_number.rb

--- 19_spreadsheet_column_number.rb
+++ 19_spreadsheet_column_number.rb
@@ -1,6 +1,20 @@
 # frozen_string_literal: false
 
 def spreadsheet(num)
-	puts num
+  alphabet = ('A'..'Z').to_a
+  str = ''
+  while num.positive?
+    rem = num % 26
+    if rem.zero?
+      str << 'Z'
+      num = (num / 26) - 1
+    else
+      str << alphabet[rem - 1]
+      num /= 26
+    end
+  end
+
+  str.reverse
 end
 
+puts spreadsheet(52)
