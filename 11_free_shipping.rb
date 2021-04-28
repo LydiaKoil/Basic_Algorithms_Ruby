4b2832947042c334ae45625b71c7c86d7e0dea78
Lydtk <47219609+Lydtk@users.noreply.github.com>
2021-04-28 22:52:51 +0100

    refactored further

--- 11_free_shipping.rb
+++ 11_free_shipping.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
 
 def free_shipping?(hash)
-  sum = 0
-  hash.each { |_, value| sum += value }
-  sum > 50
+  hash.values.inject(:+) > 50
 end
+
+puts free_shipping?({ 'Shampoo' => 5.99, 'Rubber Ducks' => 15.99 })
+puts free_shipping?({ 'Flatscreen TV' => 399.99 })
 puts free_shipping?({ 'Monopoly' => 11.99, 'Secret Hitler' => 35.99, 'Bananagrams' => 13.99 })
